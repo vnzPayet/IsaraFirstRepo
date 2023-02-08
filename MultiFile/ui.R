0ed8344be9cea4100f58dbbba0faff3b84077d5a
Vincent Payet <vpayet@isara.fr>
2023-02-08 11:55:43 +0100

    packages shinythemes

--- MultiFile/ui.R
+++ MultiFile/ui.R
@@ -1,9 +1,10 @@
 library(shiny)
+library(shinythemes)
 
 # Define UI with TAB 
 fluidPage(theme=shinytheme("sandstone"),
           navbarPage("Test d'onglet",
-                  tabPanel("Onglet1", source("tab_1.R", local = TRUE)$value),
-                  tabPanel("Onglet2", source("tab_2.R", local = TRUE)$value),
+                  tabPanel("Onglet1", source("onglets/tab_1.R", local = TRUE)$value),
+                  tabPanel("Onglet2", source("onglets/tab_2.R", local = TRUE)$value),
           )
 )
